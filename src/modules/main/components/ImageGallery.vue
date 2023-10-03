01ed3b6a76501fe596d67ddf02b242b49862d6d8
Luiz Humberto <luizhumberto@gmail.com>
2023-10-03 18:45:55 -0300

    refactor: apply iregular grid

--- src/modules/main/components/ImageGallery.vue
+++ src/modules/main/components/ImageGallery.vue
@@ -6,23 +6,52 @@ interface Props {
   images: ImagesResults | undefined
 }
 defineProps<Props>()
+
+const imageLayout = [
+  { cols: 4 },
+  {
+    cols: 4,
+    //children: [{ cols: 12 }, { cols: 12 }],
+  },
+  { cols: 4 },
+  {
+    cols: 3,
+
+    //children: [{ cols: 12 }, { cols: 12 }]
+  },
+  { cols: 6 },
+  { cols: 3 },
+  { cols: 6 },
+  { cols: 2 },
+  { cols: 4 },
+  { cols: 5 },
+  { cols: 4 },
+  { cols: 3 },
+  { cols: 4 },
+  { cols: 8 },
+  { cols: 6 },
+]
 </script>
 
 <template>
-  <v-row
-    v-if="images"
-    dense
-    justify="center"
-  >
-    <v-col
-      v-for="photo in images.photos"
-      :key="photo.id"
-      class="d-flex child-flex justify-center"
-      cols="6"
-      md="3"
-      sm="4"
+  <v-sheet max-width="1024">
+    <v-row
+      v-if="images"
+      dense
+      justify="center"
     >
-      <ImageContainer :photo="photo" />
-    </v-col>
-  </v-row>
+      <template
+        v-for="(photo, index) in images.photos"
+        :key="photo.id"
+      >
+        <v-col
+          class="d-flex child-flex justify-center"
+          cols="6"
+          :md="imageLayout[index].cols"
+        >
+          <ImageContainer :photo="photo" />
+        </v-col>
+      </template>
+    </v-row>
+  </v-sheet>
 </template>
