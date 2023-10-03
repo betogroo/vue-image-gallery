0d97a12a790f88e0ccda4a13366e1a245f002144
Luiz Humberto <luizhumberto@gmail.com>
2023-10-02 21:48:09 -0300

    chore: create search text field in app bar

--- src/shared/components/app/AppBar.vue
+++ src/shared/components/app/AppBar.vue
@@ -1,32 +1,70 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useMainStore } from '@/modules/main/store/useMainStore'
+import { useDisplay } from 'vuetify/lib/framework.mjs'
+const { mobile } = useDisplay()
 const store = useMainStore()
 const { name } = store
+const searchText = ref('')
+const handleSearch = () => {
+  console.log(searchText.value)
+}
 </script>
 <template>
   <v-app-bar
     color="blue"
-    density="compact"
+    :density="mobile ? 'prominent' : 'compact'"
   >
-    <v-app-bar-title>
-      <RouterLink
-        class="text-white text-decoration-none"
-        :to="{ name: 'HomeView' }"
-        >{{ name }}</RouterLink
+    <v-row
+      align="center"
+      class="mx-3 text-center"
+      dense
+    >
+      <v-col
+        class="text-left"
+        cols="6"
+        md="4"
       >
-    </v-app-bar-title>
-    <div class="ma-3">
-      <RouterLink
-        class="text-white text-decoration-none"
-        :to="{ name: 'HomeView' }"
-        >Home</RouterLink
+        <v-app-bar-title>
+          <RouterLink
+            class="text-white text-decoration-none"
+            :to="{ name: 'HomeView' }"
+            >{{ name }}</RouterLink
+          >
+        </v-app-bar-title>
+      </v-col>
+      <v-col
+        :cols="mobile ? '12' : ''"
+        :order="mobile ? 'last' : ''"
       >
-      |
-      <RouterLink
-        class="text-white text-decoration-none"
-        :to="{ name: 'AboutView' }"
-        >About</RouterLink
-      >
-    </div>
+        <v-text-field
+          v-model="searchText"
+          append-inner-icon="mdi-magnify"
+          center-affix
+          density="compact"
+          hide-details
+          label="Procurar Imagens"
+          placeholder="Digite um termo para a pesquisa"
+          prepend-inner-icon="mdi-image-search"
+          variant="solo-filled"
+          @click:append-inner="handleSearch"
+        />
+      </v-col>
+      <v-col md="3">
+        <div class="ma-3 text-right">
+          <RouterLink
+            class="text-white text-decoration-none"
+            :to="{ name: 'HomeView' }"
+            >Home</RouterLink
+          >
+          |
+          <RouterLink
+            class="text-white text-decoration-none"
+            :to="{ name: 'AboutView' }"
+            >About</RouterLink
+          >
+        </div>
+      </v-col>
+    </v-row>
   </v-app-bar>
 </template>
