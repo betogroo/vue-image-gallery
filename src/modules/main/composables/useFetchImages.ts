389b66555ad7219d5d2e3c3f6f3455be32ab8d21
Luiz Humberto <luizhumberto@gmail.com>
2023-10-04 16:18:29 -0300

    fix: watch a route.params instead props

--- src/modules/main/composables/useFetchImages.ts
+++ src/modules/main/composables/useFetchImages.ts
@@ -21,9 +21,7 @@ const useFetchImages = () => {
     }
   }
 
-  const fetchImages = async (
-    url: string,
-  ): Promise<ImagesResults | undefined> => {
+  const fetchImages = async (url: string) => {
     try {
       error.value = null
       isPending.value = true
@@ -32,13 +30,12 @@ const useFetchImages = () => {
           Authorization: import.meta.env.VITE_PEXELS_API_KEY,
         },
       })
-      if (!res.ok) throw new Error('Erro ao carregar as images')
+      if (!res.ok) throw new Error('Erro ao carregar as imagens')
       const imageResults: ImagesResults = await res.json()
 
       const parsedData = ImagesSchemaWithPhotos.parse(imageResults)
       if (parsedData.total_results === 0) return undefined
       photos.value = parsedData
-      return parsedData
     } catch (err) {
       const e = err as Error
       error.value = e.message
--- src/modules/main/views/ResultsView.vue
+++ src/modules/main/views/ResultsView.vue
@@ -3,24 +3,26 @@ import { ref, watch } from 'vue'
 import ImageGallery from '../components/ImageGallery.vue'
 import useFetchImages from '../composables/useFetchImages'
 import { toRefs } from 'vue'
+import { useRoute } from 'vue-router'
 const props = defineProps<Props>()
+const route = useRoute()
 const { fetchImages, photos, url, baseUrl, buildUrl } = useFetchImages()
 interface Props {
   term: string
   page: number
 }
 const { term, page } = toRefs(props)
-//buildUrl(term.value, page.value)
 
-if (!url.value) {
-  buildUrl(term.value, page.value)
-  console.log(url.value)
-  await fetchImages(url.value!)
-}
+//if (!url.value) {
+buildUrl(term.value, page.value)
+console.log(url.value)
+await fetchImages(url.value!)
+//}
 watch(
-  () => props.term,
-  async (newUrl) => {
-    buildUrl(newUrl, page.value)
+  () => route.params,
+  async () => {
+    console.log(term.value)
+    buildUrl(term.value, page.value)
     await fetchImages(url.value!)
   },
 )
@@ -32,6 +34,8 @@ watch(
       <p>page {{ page }}</p>
       <p>term {{ term }}</p>
       {{ url }}
+      <h2>{{ route.params }}</h2>
+      <h2>{{ term }}</h2>
       <h1>Resultados para {{ term }}, mostrando página {{ page }}</h1>
       <ImageGallery :images="photos" />
     </v-responsive>
