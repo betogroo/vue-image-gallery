1667b7d1ad531af21a43fdc1161cf1f8da1a8583
Luiz Humberto <luizhumberto@gmail.com>
2023-10-05 21:45:42 -0300

    refactor: pagination is splited

--- src/modules/main/components/PaginationContainer.vue
+++ src/modules/main/components/PaginationContainer.vue
@@ -1,5 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ImagesResults } from '@/shared/models/Images'
+import { toRefs } from 'vue'
+import { useRouter } from 'vue-router'
+const props = defineProps<Props>()
+const router = useRouter()
+interface Props {
+  photos: ImagesResults
+}
+const { photos } = toRefs(props)
+
+let totalPages: number
+if (photos.value)
+  totalPages =
+    photos.value.total_results % photos.value.per_page
+      ? Math.ceil(photos.value.total_results / photos.value.per_page)
+      : photos.value.total_results / photos.value.per_page + 1
+</script>
 
 <template>
-  <div></div>
+  <v-row
+    class="px-4"
+    dense
+  >
+    <v-col v-if="photos.page > 1">
+      <v-pagination
+        active-color="blue"
+        border
+        class="px-4"
+        density="comfortable"
+        :length="totalPages"
+        :model-value="photos?.page"
+        size="large"
+        variant="text"
+        @next="router.push(`?page=${photos?.page! + 1}`)"
+        @prev="router.push(`?page=${photos?.page! - 1}`)"
+        @update:model-value="(item) => router.push(`?page=${item}`)"
+      ></v-pagination>
+    </v-col>
+    <v-col
+      v-else
+      class="text-center"
+    >
+      <RouterLink :to="'?page=2'"> Ver mais Imagens</RouterLink>
+    </v-col>
+  </v-row>
 </template>
--- src/modules/main/views/HomeView.vue
+++ src/modules/main/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { watch } from 'vue'
-import ImageGallery from '../components/ImageGallery.vue'
+import { ImageGallery, PaginationContainer } from '../components'
 import useFetchImages from '../composables/useFetchImages'
-import { useRouter } from 'vue-router'
+
 const props = defineProps<Props>()
-const router = useRouter()
 import { toRefs } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
@@ -14,35 +13,25 @@ interface Props {
   page: number
 }
 const { term, page } = toRefs(props)
-
 await fetchImages(term.value, page.value)
+
 watch(
   () => route.params,
   async () => {
     await fetchImages(term.value, page.value)
   },
 )
-const totalPages =
-  photos.value!.total_results % photos.value!.per_page
-    ? Math.ceil(photos.value!.total_results / photos.value!.per_page)
-    : photos.value!.total_results / photos.value!.per_page + 1
 </script>
 
 <template>
   <v-container>
-    <v-responsive>
+    <v-responsive v-if="photos">
       <h1 v-if="term !== 'curated'">
         Resultados para {{ term }}, mostrando página {{ page }}
       </h1>
       <ImageGallery :images="photos" />
-      <v-pagination
-        class="my-4"
-        :length="totalPages"
-        :model-value="photos?.page"
-        @next="router.push(`?page=${photos?.page! + 1}`)"
-        @prev="router.push(`?page=${photos?.page! - 1}`)"
-        @update:model-value="(item) => router.push(`?page=${item}`)"
-      ></v-pagination>
+      <PaginationContainer :photos="photos" />
     </v-responsive>
+    <v-responsive v-else>Algo deu Errado</v-responsive>
   </v-container>
 </template>
